b5a62d5a017363a91630d3d23c03990721b9c12e
Gabriel Amorim <ricksamann@gmail.com>
2024-04-10 17:14:40 -0300

    req 02

---
+++ src/models/User.js
@@ -0,0 +1,27 @@
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    id: {
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER
+    },
+    displayName: {
+      field: 'display_name',
+      type: DataTypes.STRING,
+    },
+    email: {
+      type: DataTypes.STRING,
+    },
+    password: {
+      type: DataTypes.STRING,
+    },
+    image: {
+      type: DataTypes.STRING,
+    },
+  }, {
+    tableName: 'users',
+    underscored: true,
+  })
+
+  return User;
+}
